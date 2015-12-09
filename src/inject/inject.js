5469dd567faba7de832600003fa2c35c92d4f2fc
dany1468 <dany1468@gmail.com>
2015-12-09 09:20:07 +0900

    first commit

---
+++ src/inject/inject.js
@@ -0,0 +1,5 @@
+$(function() {
+	$('.btn.js-merge-branch-action')
+			.html('<img class="emoji" title=":fire:" alt=":fire:" src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f525.png" height="20" width="20" align="absmiddle"> 熱上げ <img class="emoji" title=":fire:" alt=":fire:" src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f525.png" height="20" width="20" align="absmiddle">')
+      .css('background-image', 'linear-gradient(#FF0000, #FF4500)');
+});
